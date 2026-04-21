c2b0cd610790094c3418e306e403e53b0fb9ce16
starlings-interactive <35136441+joelmartin-dev@users.noreply.github.com>
2026-04-21 18:31:22 +0930

    material skeleton

--- src/gltf_loader/mod.rs
+++ src/gltf_loader/mod.rs
@@ -265,7 +265,119 @@ pub struct Image {
   extensions: Vec<Extension>,
   extras: Vec<Extra>,
 }
-pub struct Material {}
+
+// Reference to a texture.
+pub struct TextureInfo {
+  // The index of the texture.
+  index: i32, // min: 0
+  // This integer value is used to construct a string in the format `TEXCOORD_<set index>` 
+  // which is a reference to a key in `mesh.primitives.attributes` (e.g. a value of `0` corresponds to `TEXCOORD_0`). 
+  // A mesh primitive **MUST** have the corresponding texture coordinate attributes for the material to be applicable to it.
+  tex_coord: Option<i32>, // min: 0, default: 0
+  extensions: Vec<Extension>,
+  extras: Vec<Extra>,
+}
+pub enum MaterialAlphaMode {
+  // The alpha value is ignored, and the rendered output is fully opaque.
+  OPAQUE,
+  // The rendered output is either fully opaque or fully transparent depending on the alpha value and the specified `alphaCutoff` value; 
+  // the exact appearance of the edges **MAY** be subject to implementation-specific techniques such as "Alpha-to-Coverage".
+  MASK,
+  // The alpha value is used to composite the source and destination areas. 
+  // The rendered output is combined with the background using the normal painting operation (i.e. the Porter and Duff over operator).
+  BLEND,
+  UNDEFINED
+}
+// A set of parameter values that are used to define the metallic-roughness material model from Physically-Based Rendering (PBR) methodology.
+pub struct MaterialPbrMetallicRoughness {
+  // The factors for the base color of the material. This value defines linear multipliers for the sampled texels of the base color texture.
+  base_color_factor: Option<Vec<f32 /* min: 0.0, max: 1.0 */>>, // minItems: 4, maxItems: 4, default: [ 1.0, 1.0, 1.0, 1.0 ]
+  // The base color texture. The first three components (RGB) **MUST** be encoded with the sRGB transfer function. 
+  // They specify the base color of the material. 
+  // If the fourth component (A) is present, it represents the linear alpha coverage of the material. 
+  // Otherwise, the alpha coverage is equal to `1.0`. The `material.alphaMode` property specifies how alpha is interpreted. 
+  // The stored texels **MUST NOT** be premultiplied. When undefined, the texture **MUST** be sampled as having `1.0` in all components.
+  base_color_texture: Option<TextureInfo>,
+  // The factor for the metalness of the material. 
+  // This value defines a linear multiplier for the sampled metalness values of the metallic-roughness texture.
+  metallic_factor: Option<f32>, // min: 0.0, max: 1.0, default: 1.0
+  // The factor for the roughness of the material. 
+  // This value defines a linear multiplier for the sampled roughness values of the metallic-roughness texture.
+  roughness_factor: Option<f32>, // min: 0.0, max: 1.0, default: 1.0
+  // The metallic-roughness texture. The metalness values are sampled from the B channel. 
+  // The roughness values are sampled from the G channel. These values **MUST** be encoded with a linear transfer function. 
+  // If other channels are present (R or A), they **MUST** be ignored for metallic-roughness calculations. 
+  // When undefined, the texture **MUST** be sampled as having `1.0` in G and B components.
+  metallic_roughness_texture: Option<TextureInfo>,
+  extensions: Vec<Extension>,
+  extras: Vec<Extra>,
+}
+pub struct MaterialOcclusionTextureInfo {
+  // The index of the texture.
+  index: i32, // min: 0
+  // This integer value is used to construct a string in the format `TEXCOORD_<set index>` 
+  // which is a reference to a key in `mesh.primitives.attributes` (e.g. a value of `0` corresponds to `TEXCOORD_0`). 
+  // A mesh primitive **MUST** have the corresponding texture coordinate attributes for the material to be applicable to it.
+  tex_coord: Option<i32>, // min: 0, default: 0
+  // A scalar parameter controlling the amount of occlusion applied. 
+  // A value of `0.0` means no occlusion. A value of `1.0` means full occlusion. 
+  // This value affects the final occlusion value as: `1.0 + strength * (<sampled occlusion texture value> - 1.0)`.
+  strength: Option<f32>, // min: 0.0, max: 1.0, default: 1.0
+  extensions: Vec<Extension>,
+  extras: Vec<Extra>,
+}
+pub struct MaterialNormalTextureInfo {
+  // The index of the texture.
+  index: i32, // min: 0
+  // This integer value is used to construct a string in the format `TEXCOORD_<set index>` 
+  // which is a reference to a key in `mesh.primitives.attributes` (e.g. a value of `0` corresponds to `TEXCOORD_0`). 
+  // A mesh primitive **MUST** have the corresponding texture coordinate attributes for the material to be applicable to it.
+  tex_coord: Option<i32>, // min: 0, default: 0
+  // The scalar parameter applied to each normal vector of the texture. 
+  // This value scales the normal vector in X and Y directions using the formula: 
+  // `scaledNormal =  normalize((<sampled normal texture value> * 2.0 - 1.0) * vec3(<normal scale>, <normal scale>, 1.0))`.
+  scale: Option<f32>, // default: 1.0
+  extensions: Vec<Extension>,
+  extras: Vec<Extra>,
+}
+// The material appearance of a primitive.
+pub struct Material {
+  // A set of parameter values that are used to define the metallic-roughness material model from Physically Based Rendering (PBR) methodology. 
+  // When undefined, all the default values of `pbrMetallicRoughness` **MUST** apply.
+  pbr_metallic_roughness: Option<MaterialPbrMetallicRoughness>,
+  // The tangent space normal texture. The texture encodes RGB components with linear transfer function. 
+  // Each texel represents the XYZ components of a normal vector in tangent space. 
+  // The normal vectors use the convention +X is right and +Y is up. +Z points toward the viewer. 
+  // If a fourth component (A) is present, it **MUST** be ignored. When undefined, the material does not have a tangent space normal texture.
+  normal_texture: Option<MaterialNormalTextureInfo>,
+  // The occlusion texture. The occlusion values are linearly sampled from the R channel. 
+  // Higher values indicate areas that receive full indirect lighting and lower values indicate no indirect lighting. 
+  // If other channels are present (GBA), they **MUST** be ignored for occlusion calculations. 
+  // When undefined, the material does not have an occlusion texture.
+  occlusion_texture: Option<MaterialOcclusionTextureInfo>,
+  // The emissive texture. It controls the color and intensity of the light being emitted by the material. 
+  // This texture contains RGB components encoded with the sRGB transfer function. 
+  // If a fourth component (A) is present, it **MUST** be ignored. 
+  // When undefined, the texture **MUST** be sampled as having `1.0` in RGB components.
+  emissive_texture: Option<TextureInfo>,
+  // The factors for the emissive color of the material. 
+  // This value defines linear multipliers for the sampled texels of the emissive texture.
+  emissive_factor: Option<Vec<f32 /* min: 0.0, max: 1.0 */>>, // minItems: 3, maxItems: 3, default: [ 0.0, 0.0, 0.0 ]
+  // The material's alpha rendering mode enumeration specifying the interpretation of the alpha value of the base color.
+  alpha_mode: Option<String>, // OPAQUE, MASK, BLEND, or some string. default: OPAQUE
+  // Specifies the cutoff threshold when in `MASK` alpha mode. 
+  // If the alpha value is greater than or equal to this value then it is rendered as fully opaque, otherwise, it is rendered as fully transparent. 
+  // A value greater than `1.0` will render the entire material as fully transparent. 
+  // This value **MUST** be ignored for other alpha modes. When `alphaMode` is not defined, this value **MUST NOT** be defined.
+  alpha_cutoff: Option<f32>, // min: 0.0, default: 0.5
+  // Specifies whether the material is double sided. When this value is false, back-face culling is enabled. 
+  // When this value is true, back-face culling is disabled and double-sided lighting is enabled. 
+  // The back-face **MUST** have its normals reversed before the lighting equation is evaluated.
+  double_sided: Option<bool>, // default: false
+  name: Option<String>,
+  extensions: Vec<Extension>,
+  extras: Vec<Extra>,
+}
 // Geometry to be rendered with the given material.
 pub enum MeshPrimitiveMode {
   POINTS = 0,
@@ -408,17 +520,6 @@ pub struct Texture {
   // When undefined, an extension or other mechanism **SHOULD** supply an alternate texture source, otherwise behavior is undefined.
   source: Option<i32>, // min: 0
   name: Option<String>,
-  extensions: Vec<Extension>,
-  extras: Vec<Extra>,
-}
-// Reference to a texture.
-pub struct TextureInfo {
-  // The index of the texture.
-  index: i32, // min: 0
-  // This integer value is used to construct a string in the format `TEXCOORD_<set index>` 
-  // which is a reference to a key in `mesh.primitives.attributes` (e.g. a value of `0` corresponds to `TEXCOORD_0`). 
-  // A mesh primitive **MUST** have the corresponding texture coordinate attributes for the material to be applicable to it.
-  tex_coord: Option<i32>, // min: 0, default: 0
   extensions: Vec<Extension>,
   extras: Vec<Extra>,
 }
