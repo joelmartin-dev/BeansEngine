3cf27c65c47cfb480399153cb7618e1d507349c9
joelmartin-dev <joel.martin02@bigpond.com>
2026-04-23 13:10:13 +0930

    test loads path to models through envvar, no meshopt support yet

--- src/gltf_loader/methods.rs
+++ src/gltf_loader/methods.rs
@@ -1,10 +1,10 @@
 use crate::gltf_loader::GltfLoader;
 
-impl GltfLoader {
-  pub fn new() -> Self 
-  {
-    Self {
+// impl GltfLoader {
+//   pub fn new() -> Self 
+//   {
+//     Self {
 
-    }
-  }
-}
+//     }
+//   }
+// }
